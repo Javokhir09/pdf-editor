c9700fe0b796f0ef56f8d9e7e84d18fb22256339
Javokhir09 <javokhirturdikulov@gmail.com>
2026-07-20 10:39:17 +0500

    styled editor component and new toggle button component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,29 +1,28 @@
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import ToggleButton from "./ToggleButton"
+import Placeholder from "@tiptap/extension-placeholder"
 
 function Editor() {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "Hello, World!"
+    extensions: [
+      StarterKit,
+      Placeholder.configure({placeholder: "Write here...",})
+    ],
+    
   })
 
   if (!editor) return null
 
   return (
-    <div>
-      <div className="tools">
-        <button onClick={() => editor.chain().focus().toggleBold().run()}>
-          B
-        </button>
-        <button onClick={() => editor.chain().focus().toggleItalic().run()}>
-          I
-        </button>
-        <button onClick={() => editor.chain().focus().toggleUnderline().run()}>
-          U
-        </button>
+    <div className="w-full flex items-center flex-col gap-2 p-2">
+      <div className="flex gap-2">
+        <ToggleButton onClick={() => editor.chain().focus().toggleBold().run()} className="font-bold">B</ToggleButton>
+        <ToggleButton onClick={() => editor.chain().focus().toggleItalic().run()} className="italic">I</ToggleButton>
+        <ToggleButton onClick={() => editor.chain().focus().toggleUnderline().run()} className="underline">U</ToggleButton>
       </div>
-      <div className="editor">
-        <EditorContent editor={editor} />
+      <div className="w-full h-[calc(100vh-52px)] flex justify-center">
+        <EditorContent editor={editor} className="w-1/2" />
       </div>
     </div>
   )
--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,7 +1,17 @@
-function ToggleButton() {
+import { useState, type ButtonHTMLAttributes } from "react"
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+function ToggleButton({ children, className = "", ...props }: ButtonProps) {
+  const [toggled, setToggled] = useState<boolean>(false)
+
   return (
-    <button>
-      
+    <button 
+      {...props}
+      className={`border border-black/30 size-7 cursor-pointer ${toggled ? "bg-gray-200/80" : ""} ${className}`} 
+      onClick={(e) => {setToggled((prev) => !prev); props.onClick?.(e)}} 
+    >
+      {children}
     </button>
   )
 }
